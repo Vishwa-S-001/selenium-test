9fc6d691b4a5065d1e67d2cd255bde609830825d
Vishwa S <81308757+Vishwa-S-001@users.noreply.github.com>
2024-07-11 21:39:53 +0530

    Delete TestAssignment/src/main/java directory
    deleted the created pages

--- TestAssignment/src/main/java/pages/supportcenter/CreateTicketPage.java
+++
@@ -1,7 +0,0 @@
-package main.java.weblocators.supportcenter;
-
-public interface CreateTicketPage {
-    final String subject = "input#id_subject";
-    final String message = "div.hf-ticket-message-container  textarea";
-    final String createTicketBtn = "button#submit";
-}
